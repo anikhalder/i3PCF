8c827cd90720643ebc2938a90ca3f443812f6cfb
Anik Halder <halder.anik04@gmail.com>
2024-06-08 23:29:57 +0200

    Update neutrino mass functions

--- src/neutrino_utils.py
+++ src/neutrino_utils.py
@@ -1,21 +1,15 @@
-import numpy as np 
-import astropy.units as u
-from astropy.cosmology import FlatLambdaCDM
 from scipy.optimize import fsolve
-
-######################################################################################
 
 ## function to compute individual masses of massive neutrinos
 
-def get_masses(delta_m_squared_atm, delta_m_squared_sol, sum_masses, hierarchy):
+def get_masses(sum_masses, hierarchy, delta_m_squared_atm=2.5e-3, delta_m_squared_sol=7.37e-5):
     """ Return individual masses of neutrinos
 
     Args:
-        delta_m_squared_atm ( float ): squared difference of neutrino masses from atmospheric oscillation experiments
-        delta_m_squared_sol ( float ): squared difference of neutrino masses from solar oscillation experiments
         sum_masses ( float ): sum of neutrino masses
         hierarchy ( string ): Neutrino hierarchy
-
+        delta_m_squared_atm ( float ): squared difference of neutrino masses from atmospheric oscillation experiments
+        delta_m_squared_sol ( float ): squared difference of neutrino masses from solar oscillation experiments
     Returns:
         m1, m2, m3: Individual neutrino masses
     """
@@ -27,8 +21,14 @@ def get_masses(delta_m_squared_atm, delta_m_squared_sol, sum_masses, hierarchy):
         m3 = 0.0
         return m1, m2, m3
     
-    # any string containing letter 'n' will be considered as refering to normal hierarchy
-    if ('n' in hierarchy.lower()):
+    if (hierarchy == 'DEGENERATE'):
+        # degenerate neutrino case
+        m1 = sum_masses / 3
+        m2 = sum_masses / 3
+        m3 = sum_masses / 3
+        return m1, m2, m3
+    
+    elif (hierarchy == 'NORMAL'):
         # delta_m_squared_atm = m_3^2 - (m_1^2 + m_2^2)/2
         # delta_m_squared_sol = m_2^2 - m_1^2
         m1_func = lambda m1, M_tot, d_m_sq_atm, d_m_sq_sol: M_tot**2. + 0.5*d_m_sq_sol - d_m_sq_atm + m1**2. - 2.*M_tot*m1 - 2.*M_tot*(d_m_sq_sol+m1**2.)**0.5 + 2.*m1*(d_m_sq_sol+m1**2.)**0.5
@@ -37,7 +37,8 @@ def get_masses(delta_m_squared_atm, delta_m_squared_sol, sum_masses, hierarchy):
         m2 = (delta_m_squared_sol + m1**2.)**0.5
         m3 = (delta_m_squared_atm + 0.5*(m2**2. + m1**2.))**0.5
         return m1, m2, m3
-    else:
+    
+    elif (hierarchy == 'INVERTED'):
         # Inverted hierarchy massive neutrinos. Calculates the individual
         # neutrino masses from M_tot_IH and deletes M_tot_IH
         #delta_m_squared_atm = -m_3^2 + (m_1^2 + m_2^2)/2
@@ -50,13 +51,5 @@ def get_masses(delta_m_squared_atm, delta_m_squared_sol, sum_masses, hierarchy):
         m3 = (delta_m_squared_atm + 0.5*(m2**2. + m1**2.))**0.5
         return m1, m2, m3
     
-######################################################################################
-
-## function to compute density parameter of massive neutrinos
-
-def Mnu2omeganu(sum_masses, Omega_m_0, Omega_b_0, h):
-    # the atmospheric and solar neutrino mass constraints are from the MassiveNuS paper by Liu et al.
-    m1, m2, m3 = get_masses(2.5e-3, 7.37e-5, sum_masses, 'NH') 
-    mnu_arr = np.array([m1, m2, m3])* u.eV
-    cosmo = FlatLambdaCDM(H0=h*100, Om0=Omega_m_0, Neff=3.046, m_nu=mnu_arr, Ob0=Omega_b_0, Tcmb0=2.7255)
-    return cosmo.Onu0*(h**2)
+    else:
+        raise ValueError('Unknown hierarchy %s'%hierarchy)
